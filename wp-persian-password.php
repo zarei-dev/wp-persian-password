bb25bca929cccbe4e1421ea0921aac4d0b382d91
zarei.dev <mohammad.zarei1380@gmail.com>
2022-06-27 17:01:17 +0430

    faet: Add authentication for persian passwords

--- wp-persian-password.php
+++ wp-persian-password.php
@@ -25,3 +25,80 @@
 if ( !defined( 'ABSPATH' ) ) {
 	die( 'We\'re sorry, but you can not directly access this file.' );
 }
+
+add_filter( 'authenticate', 'wp_authenticate_persian_password', 30, 3 );
+
+function wp_authenticate_persian_password( $user, $email_or_username, $password ) {
+    if ( $user instanceof WP_User ) {
+        return $user;
+    }
+
+    if ( $user instanceof WP_Error && $user->get_error_code() === 'incorrect_password' ) {
+        // If the error code is incorrect_password, we know the password is wrong.
+
+        // if email_or_username is email, then it's email address
+        if ( is_email( $email_or_username ) ) {
+            $user = get_user_by( 'email', $email_or_username );
+            if ( ! $user ) {
+                return new WP_Error(
+                    'invalid_email',
+                    __( 'Unknown email address. Check again or try your username.' )
+                );
+            }
+            $is_email = true;
+        } else {
+            $user = get_user_by( 'login', $email_or_username );
+            if ( ! $user ) {
+                return new WP_Error(
+                    'invalid_username',
+                    sprintf(
+                        /* translators: %s: User name. */
+                        __( '<strong>Error</strong>: The username <strong>%s</strong> is not registered on this site. If you are unsure of your username, try your email address instead.' ),
+                        $email_or_username
+                    )
+                );
+            }
+            $is_email = false;
+        }
+
+        $persian = ['۰', '۱', '۲', '۳', '۴', '٤', '۵', '٥', '٦', '۶', '۷', '۸', '۹'];
+        $english = [0, 1, 2, 3, 4, 4, 5, 5, 6, 6, 7, 8, 9];
+
+        $english_password = str_replace($persian, $english, $password);
+        $persian_password = str_replace($english, $persian, $password);
+
+        // We check if the password is correct with persian numbers or not.
+        if ( wp_check_password( $english_password, $user->user_pass, $user->ID ) || wp_check_password( $persian_password, $user->user_pass, $user->ID ) ) {
+            return $user;
+        }
+
+
+        if ( $is_email ) {
+            return new WP_Error(
+                'incorrect_password',
+                sprintf(
+                    /* translators: %s: Email address. */
+                    __( '<strong>Error</strong>: The password you entered for the email address %s is incorrect.' ),
+                    '<strong>' . $email_or_username . '</strong>'
+                ) .
+                ' <a href="' . wp_lostpassword_url() . '">' .
+                __( 'Lost your password?' ) .
+                '</a>'
+            );
+        } else {
+            return new WP_Error(
+                'incorrect_password',
+                sprintf(
+                    /* translators: %s: User name. */
+                    __( '<strong>Error</strong>: The password you entered for the username %s is incorrect.' ),
+                    '<strong>' . $email_or_username . '</strong>'
+                ) .
+                ' <a href="' . wp_lostpassword_url() . '">' .
+                __( 'Lost your password?' ) .
+                '</a>'
+            );
+        }
+
+    }
+        
+}

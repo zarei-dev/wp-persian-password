7f43eff5bf7826555a9e0be9796b5765769f547e
zarei.dev <mohammad.zarei1380@gmail.com>
2022-06-27 16:27:27 +0430

    feat: Add plugin header

---
+++ wp-persian-password.php
@@ -0,0 +1,27 @@
+
+
+<?php
+
+/**
+ * @package   Persian Password
+ * @author    mohammad.zarei1380@gmail.com <{{author_email}}>
+ * @license   GPL-3.0+
+ * @link      {{author_link}}
+ *
+ * Plugin Name:     Persian Password
+ * Plugin URI:      https://zarei.dev
+ * Description:     Accept Password with persian numbers in your WordPress Login form.
+ * Version:         0.0.1
+ * Author:          mohammad.zarei1380@gmail.com
+ * Author URI:      https://zarei.dev
+ * Text Domain:     persian-password
+ * License:         GPL-3.0+
+ * License URI:     http://www.gnu.org/licenses/gpl-3.0.txt
+ * Domain Path:     /languages
+ * Requires PHP:    5.6
+ */
+
+// If this file is called directly, abort.
+if ( !defined( 'ABSPATH' ) ) {
+	die( 'We\'re sorry, but you can not directly access this file.' );
+}
